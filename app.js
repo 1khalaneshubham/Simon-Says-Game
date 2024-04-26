705a0ca9da5e60325f82c2516a19b063a3241f08
Mr_Shubham_Khalane <118313700+1khalaneshubham@users.noreply.github.com>
2024-04-26 13:35:30 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,72 @@
+let gameSeq = [];
+let userSeq = [];
+
+let btns = ["yellow","red","purple","green"];
+
+let started = false;
+let level = 0;
+
+let h2 = document.querySelector("h2");
+
+document.addEventListener("keypress",function() {
+    if(started == false) {
+        console.log("game is started");
+        started = true;
+
+        levelUp();
+    }
+});
+
+function btnFlash(btn){
+    btn.classList.add("flash");
+    setTimeout(function(){
+        btn.classList.remove("flash");
+    },1000);
+}
+
+function levelUp(){
+    userSeq = [];
+    level++;
+    h2.innerText = `Level ${level}`;
+
+    // random btn choose
+    let randIdx = Math.floor(Math.random()*3);
+    let randColor = btns[randIdx];
+    let randBtn = document.querySelector(`.${randColor}`);
+    gameSeq.push(randColor);
+    console.log(gameSeq);
+    gameFlash(randBtn);
+}
+
+function checkAns(idx) {
+    // console.log("curr level :",level);
+    // let idx = level-1;
+    if(userSeq[idx] === gameSeq[idx]){
+        // console.log("same value");
+        if(userSeq.length == gameSeq.length){
+            setTimeout(levelUp,1000);
+        }
+    }else{
+        h2.innerText = `Game Over! Your scoe was <b>${level}</b> Press any key to start.`;
+        document.querySelector("body").style.backgroundcolor = "red";
+        setTimeout(function(){
+            document.querySelector("body").style.backgroundcolor = "white";
+        },150);
+        reset();
+    }
+}
+
+function btnPress() {
+    console.log(this);
+    let btn = this;
+    userFlash(btn);
+
+    userColor = btn.getAttribute("id");
+    userSeq.push(userColor);
+    checkAns(userSeq.length-1);
+}
+
+let allBtns = document.querySelectorAll(".btn");
+for(btn of allBtns){
+    btn.addEventListener("click",btnPress);
+}
